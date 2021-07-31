0d4a9e8785b004b8f48996ed373da8d57d44228a
Senthilnadan Jothi <sriram.senthilnadan@gmail.com>
2021-08-01 00:22:14 +0530

    pokeymanCard

---
+++ src/components/pokemanCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let pokeman;
+</script>
+<a href={`/pokeman/{pokeman.id}`}>
+    <img src={pokeman.image} alt={pokeman.name} >
+   <h2>{pokeman.id}. {pokeman.name}</h2>
+
+</a>
+{
+    pokeman.name
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
     import {pokemon} from "../stores/pokestore"
+    import PokemanCard from "../components/pokemanCard.svelte"
     console.log($pokemon)
   
 </script>
@@ -10,6 +11,7 @@
 <h1 class="text-4xl text-center my-8 uppercase"> Pokedex This is a Pokedex App BY SRIRAM</h1>
 
 {#each $pokemon as pokeman}
-    <p> {pokeman.name}</p>
+    <PokemanCard pokeman={pokeman}></PokemanCard>
+   
 {/each}
 
