7ba09c0cb642872f4ccaa0c36d0ae78287b875c9
Senthilnadan Jothi <sriram.senthilnadan@gmail.com>
2021-08-01 12:50:02 +0530

    pokecard added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,37 @@
 <script>
-    import {pokemon} from "../stores/pokestore"
-    import PokemanCard from "../components/pokemanCard.svelte"
-    console.log($pokemon)
-  
+	import { pokemon } from '../stores/pokestore';
+	import PokemanCard from '../components/pokemanCard.svelte';
+	console.log($pokemon);
+
+	let searchTerm = '';
+	let filteredpokemon = [];
+
+	$: {
+		console.log(searchTerm);
+		if (searchTerm) {
+			filteredpokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().
+            includes(searchTerm.toLowerCase()));
+		} else {
+			filteredpokemon = [...$pokemon]
+		}
+	}
 </script>
+
 <svelte:head>
-    <title> srirams pokedex </title>
+	<title>srirams pokedex</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8 uppercase"> Pokedex This is a Pokedex App BY SRIRAM</h1>
+<h1 class="text-4xl text-center my-8 uppercase">Pokedex This is a Pokedex App BY SRIRAM</h1>
 
-<div class="py-4 grid gap-4 md:grid-cols-2 grid- cols-1">
+<input
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	bind:value={searchTerm}
+	type="text"
+	placeholder="Search Pokemon"
+/>
 
-{#each $pokemon as pokeman}
-    <PokemanCard pokeman={pokeman}></PokemanCard>
-   
-{/each} 
-
+<div>
+	{#each filteredpokemon as pokeman}
+		<PokemanCard pokeman={pokeman} />
+	{/each}
 </div>
-
